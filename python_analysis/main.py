15d79ab3ee50dc53567ed884d28bc6258cc19f3c
pomtbo <unicoder@foxmail.com>
2021-04-30 09:50:42 +0800

    卸载不需要的

--- python_analysis/main.py
+++ python_analysis/main.py
@@ -3,14 +3,24 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 打印字符
+def printX(n):
+    matrix = [[" "] * n for _ in range(n)]
+    left, righ = 0, n - 1
+    for i in range(n):
+        matrix[i][left] = 'X'
+        matrix[i][righ] = 'X'
+        left += 1
+        righ -= 1
+    result = []
+    print(matrix)
+    for i in range(n):
+        result.append("".join(matrix[i]))
+    return result
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    a = printX(5)
+    print(a)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
