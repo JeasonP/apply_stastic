15e183d10ce2c6bf51a3b1d486dadd67f8b8ec45
pomtbo <unicoder@foxmail.com>
2021-05-01 12:13:21 +0800

    r

--- python_analysis/main.py
+++ python_analysis/main.py
@@ -6,12 +6,12 @@
 # 打印字符
 def printX(n):
     matrix = [[" "] * n for _ in range(n)]
-    left, righ = 0, n - 1
+    left, right = 0, n - 1
     for i in range(n):
         matrix[i][left] = 'X'
-        matrix[i][righ] = 'X'
+        matrix[i][right] = 'X'
         left += 1
-        righ -= 1
+        right -= 1
     result = []
     print(matrix)
     for i in range(n):
@@ -19,8 +19,52 @@ def printX(n):
     return result
 
 
+def isPalindrome(n):
+    a = str(bin(n))
+    b = a[2:]
+    if n == 0:
+        return True
+    c = len(b) - 1
+    for i in range(int(c / 2)):
+        if b[i] != b[len(b) - 1]:
+            return False
+    return True
+
+
+# 回文判断
+def isPalindrome1(n):
+    a = str(bin(n))
+    b = a[2:]
+    c = b[::-1]
+    if c == b:
+        return True
+    else:
+        return False
+
+
+# 杨辉三角
+def calcYangHuisTriangle(n):
+    res = []
+    if n == 0:
+        return res
+    for i in range(n):
+        temp = [1]
+    for j in range(1, i):
+        temp.append(res[i - 1][j - 1] + res[i - 1][j])
+    if i > 0:
+        temp.append(1)
+        res.append(temp)
+    return res
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    a = printX(5)
-    print(a)
+    # a = printX(5)
+    # print(a)
+    isPalindrome1(99)
+    calcYangHuisTriangle(5)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+bin(99)
+a = bin(99)
+print(a[1])
